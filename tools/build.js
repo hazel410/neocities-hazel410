e310354eeb784fd1b826f19e072b8848b3a84066
hazel410 <wild.hazelnut.tree@gmail.com>
2026-02-13 14:50:22 -0500

    workflow test 7

--- tools/build.js
+++ tools/build.js
@@ -85,7 +85,6 @@ async function makeDir(path) {
 // THINGS USING OTHER FUNCS
 
 async function getRootDir() {
-  console.log(process.cwd());
   const rootPath = await getFileStrFromPath('./tools/root.txt');
   return rootPath;
 }
